9154fb9fa03dc6c1aa674f6b7c9130b93e08dc63
todoroal <alexander.todorovic@stud.fh-campuswien.ac.at>
2021-04-25 18:55:02 +0200

    Update Controller.java

--- src/main/java/newsanalyzer/ctrl/Controller.java
+++ src/main/java/newsanalyzer/ctrl/Controller.java
@@ -1,16 +1,40 @@
 package newsanalyzer.ctrl;
 
+import newsapi.NewsApi;
+import newsapi.beans.Article;
+import newsapi.beans.NewsReponse;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 public class Controller {
 
-	public static final String APIKEY = "myAPIKey";
+	public static final String APIKEY = "538d94982e5444b78cfe15198c4786b2";
 
-	public void process() {
+	public void process(NewsApi stream) throws IOException, NewsAnalyzerException {
 		System.out.println("Start process");
 
 		//versuchen daten von api zu bekommen getdata
 		//exception müssen beim user landen -> im userinterface
 		//wir fangen nur runtime exceptions ab , url exc,io exc.
 
+		NewsReponse newsReponse = stream.getNews();
+		if(newsReponse != null){
+			List<Article> articles = newsReponse.getArticles();
+			articles.stream().forEach(article -> System.out.println(article.toString()));
+
+			System.out.println("Number of Articles: "+ getNumberOfArticle(articles));
+			System.out.println("Best Provider: " +getBestProvider(articles));
+			System.out.println("Title sorted by lenght:" + getTitlesSortedByLenght(articles).toString());
+
+
+		}else{
+			throw new NewsAnalyzerException("No News");
+		}
 
 
 
@@ -22,9 +46,36 @@ public class Controller {
 
 		System.out.println("End process");
 	}
-	
+
+	public List<Article> getTitlesSortedByLenght(List<Article> data){
+		return data
+				.stream()
+				.sorted(Comparator.comparingInt(Article -> Article.getTitle().length()))
+				.collect(Collectors.toList());
+	}
+
+	public long getNumberOfArticle(List<Article> data){
+		return (long) data.size();
+	}
+
+	public String getBestProvider(List<Article> data){
+		return data
+				.stream()
+				.collect(Collectors.groupingBy(article -> article.getSource().getName(), Collectors.counting()))
+				.entrySet()
+				.stream()
+				.max(Map.Entry.comparingByValue()).orElseThrow(NoSuchElementException::new).getKey();
+	}
+
+	public String getShortesAuthorName(List<Article> data){
+		return data
+				.stream()
+				.min(Comparator.comparing(Article::getAuthor))
+				.toString();
+	}
 
 	public Object getData() {
+
 
 		return null;
 	}
